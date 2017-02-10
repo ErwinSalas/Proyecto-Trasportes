ccc9632e32b9481b31bfc69c08ebfb7c157212cd
ErwinSalas <erwinsalas42@gmail.com>
2017-02-10 10:25:57 -0600

    Plantilla base

---
+++ admin/services.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Erwin on 27/10/2016.
+ */
+angular.module('adminModule')
+.factory('UserResource',function($resource){
+          return $resource("http://localhost:8000/users/:id",{id:"@id"},{
+            update : {method:'PUT',params:{id:"@id"}}
+        });
+})
+.factory('RequestResource',function($resource){
+    return $resource("http://localhost:8000/requests/:id",{id:"@id"},{
+        update : {method:'PUT',params:{id:"@id"}}
+    });
+})
+.factory('PersonResource',function($resource){
+    return $resource("http://localhost:8000/persons/:id",{id:"@id"},{
+        update : {method:'PUT',params:{id:"@id"}}
+    });
+})
+.factory('AdultResource',function($resource){
+    return $resource("http://localhost:8000/adults/:id",{id:"@id"},{
+        update : {method:'PUT',params:{id:"@id"}}
+    });
+})
+.factory('ExpedientResource',function($resource){
+    return $resource("http://localhost:8000/expedients/:id",{id:"@id"},{
+        update : {method:'PUT',params:{id:"@id"}}
+    });
+});
---
+++ loginController.js
@@ -0,0 +1,41 @@
+/**
+ * Created by josah on 20 nov 2016.
+ */
+var app = angular.module('loginModule',["ngRoute","ngResource"])
+    .controller('loginController', function($scope,$http) {
+        /* config object */
+        $scope.username = "";
+        $scope.password = "";
+
+        var usersList = [];
+
+        $http({
+            method : "GET",
+            url : "http://localhost:8000/users"
+        }).then(function mySucces(response) {
+            usersList = response.data;
+            console.log(usersList);
+        });
+
+        $scope.verify = function(){
+
+            var user = $.grep(usersList, function(current){
+                return ((current.name == $scope.username) && (current.password == $scope.password))
+            });
+
+            if (user.length == 1){
+                var type = user[0].type;
+                if(type == "admin"){
+                    sessionStorage.setItem("user", JSON.stringify(user[0]));
+                    window.location.href = ('http://localhost:63342/hasc/Angular-Client/admin/MainView.html');
+                }
+                else if(type == "doctor"){
+                    sessionStorage.setItem("user", JSON.stringify(user[0]));
+                    window.location.href = ('http://localhost:63342/hasc/Angular-Client/doctors/MainView.html');
+                }
+            }
+            else{
+                alert("Usuario o contraseña incorrectos");
+            }
+        }
+});
