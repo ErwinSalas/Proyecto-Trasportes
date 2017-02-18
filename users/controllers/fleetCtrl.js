7f98a4bb8c30f8ffe8f642db4a0170d367aeba56
ErwinSalas <erwinsalas42@gmail.com>
2017-02-18 14:33:03 -0600

    Listview flotilla v1

--- users/controllers/fleetCtrl.js
+++ users/controllers/fleetCtrl.js
@@ -3,10 +3,24 @@
  */
 
 angular.module('userModule')
-    .controller('fleetIndexCtrl', function($scope,FleetResource) {
+    .controller('fleetIndexCtrl', function($scope,$http) {
         /* config object */
-        $scope.fleets = FleetResource.query();
-        console.log($scope.fleets)
+
+
+        $http.get(
+            'http://transportec.azurewebsites.net/fleet/getFleet'
+        ).success(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log("entro", response);
+            $scope.fleets = response.content;
+        }).error(function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("fallo", response);
+            $scope.fleets= response;
+        });
+        console.log( $scope.fleets)
 
 
 
--- users/services.js
+++ users/services.js
@@ -7,34 +7,34 @@ angular.module('userModule')
             update : {method:'PUT',params:{id:"@id"}}
         });
     })
-    .factory('FleetResource',function($resource){
-        return $resource("http://localhost:8000/reservations/:id",{id:"@id"},{
+    /*.factory('FleetResource',function($resource){
+        return $resource("http://transportec.azurewebsites.net/fleet/getFleet/:id",{id:"@id"},{
             update : {method:'PUT',params:{id:"@id"}}
         });
-    })
+    })*/
 
 
-    .factory('FleetService',function($http){
-        /* return $resource("http://transportec.azurewebsites.net/fleet/getFleet/:id",{id:"@id"},{
+    .factory('FleetResource',function($http){
+        /* return $resource("/:id",{id:"@id"},{
         update : {method:'PUT',params:{id:"@id"}}
         });*/
-        var getFleet=function() {
-            $http({
-                method: 'GET',
-                url: 'http://transportec.azurewebsites.net/fleet/getFleet'
-            }).then(function successCallback(response) {
+        var fleet={
+            dataV: $http.get(
+                'http://transportec.azurewebsites.net/fleet/getFleet'
+            ).success(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
-                console.log("entro", response.data.content);
-                return response.data.content;
-            }, function errorCallback(response) {
+                console.log("entro", response);
+                return response;
+            }).error(function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
                 console.log("fallo", response);
                 return response;
-            });
+            })
+        };
 
-        }
+
 
 
     });
