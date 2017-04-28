eac791543b733e8e2742acfb14dd06daf957ecca
AdrianHS <ad1190@hotmail.com>
2017-04-28 14:37:40 -0600

    Pequeños cambios

--- users/controllers/myreservesInfoCtrl.js
+++ users/controllers/myreservesInfoCtrl.js
@@ -1,24 +1,13 @@
 angular.module('userModule')
     .controller('myreservesInfoCtrl', function($scope,$routeParams,ReserveResource) {
         /* config object */
-
         $scope.valueID = $routeParams.valueID;
-
         console.log(reserveSelectedID);
         $scope.getReserve = ReserveResource.getReserve(reserveSelectedID, function (res) {
             $scope.reserve=res;
             console.log("La resInfo ", $scope.reserve);
-            if($scope.reserve.Status == 0){
-                $scope.reserve.Status = "Pendiente";
+            if($scope.reserve.AssignedDriver == null){
+                $scope.reserve.AssignedDriver = "No";
             }
-            if($scope.reserve.Status == 1){
-                $scope.reserve.Status = "Aceptado";
-            }
-            if($scope.reserve.Status == 2){
-                $scope.reserve.Status = "Denegado";
-            }
-
-
         });
-
     }); 
