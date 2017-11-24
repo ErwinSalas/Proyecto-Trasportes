bf85f47cde197a1692bf88e2c81d036e4fc6cbd8
AdrianHS <ad1190@hotmail.com>
2017-11-24 13:20:46 -0600

    - Cambios en la segunda boleta - Validaciónes restantes de crear Chofer - Validaciónes restantes de crear Flotilla - Pequeños reajustes en los HTML Flotilla y Chofer

--- admin/controllers/fleetCreateCtrl.js
+++ admin/controllers/fleetCreateCtrl.js
@@ -8,25 +8,72 @@ angular.module('adminModule')
         $scope.newCar={
             headquarter:"SanCarlos"
         };
+        
+        $scope.validateCreateFleet=function () {
+            /*-------------------VALIDACIONES---------------------*/
+            //Validacion de marca de auto
+            if($scope.newCar.brand == null || $scope.newCar.brand == ""){
+                //alert("Error, debe seleccionar digitar marca");
+                return false;
+            }
+            //Validacion de modelo de auto
+            if($scope.newCar.model == null || $scope.newCar.model == ""){
+                //alert("Error, debe seleccionar digitar modelo");
+                return false;
+            }
+            //Validacion de placa de auto
+            if($scope.newCar.vehicleId == null || $scope.newCar.vehicleId == ""){
+                //alert("Error, debe seleccionar digitar los placa");
+                return false;
+            }
+            //Validacion de transmision de auto
+            if($scope.newCar.traction == null || $scope.newCar.traction == ""){
+                //alert("Error, debe seleccionar digitar los transmision");
+                return false;
+            }
+            //Validacion de capacidad de auto
+            if($scope.newCar.capacity == null || $scope.newCar.capacity == ""){
+                //alert("Error, debe seleccionar digitar la capacidad");
+                return false;
+            }
+            //Validacion de placa de auto
+            if($scope.newCar.mileage == null || $scope.newCar.mileage == ""){
+                //alert("Error, debe seleccionar digitar el kilometraje");
+                return false;
+            }
+            //Validacion de dependencia de auto
+            if($scope.newCar.dependence == null || $scope.newCar.dependence == ""){
+                //alert("Error, debe seleccionar digitar el dependencia");
+                return false;
+            }
+        };
+        
         $scope.postCar=function() {
-            console.log("enviPo",$scope.newCar);
-            var isPost = FleetCarResources.setNewCar($scope.newCar, function (res) {
-                isPost=res;
-            });
-
-            responseData=MediaResource.setImg($scope.img,$scope.newCar.vehicleId,2);
-            
-            swal({
-                title: "Vehiculo agregado",
-                type: "success",
-                confirmButtonColor: "#140e39",
-                timer: 2000,
-                showConfirmButton: false
-            });
-            $timeout( function(){
-                window.location.href = '#/admin/fleetAdmin';
-            }, 2000 );
-
+            if(validateCreateFleet()){
+                console.log("enviPo",$scope.newCar);
+                var isPost = FleetCarResources.setNewCar($scope.newCar, function (res) {
+                    isPost=res;
+                });
+                responseData=MediaResource.setImg($scope.img,$scope.newCar.vehicleId,2);
+                swal({
+                    title: "Vehiculo agregado",
+                    type: "success",
+                    confirmButtonColor: "#140e39",
+                    timer: 2000,
+                    showConfirmButton: false
+                });
+                $timeout( function(){
+                    window.location.href = '#/admin/fleetAdmin';
+                }, 2000 );
+            }else {
+                swal({
+                    title: "Campos vacios!",
+                    text: "El espacio no puede ir en blanco",
+                    timer: 2000,
+                    type: "error",
+                    showConfirmButton: false
+                });
+            }
         };
 
         function urltoFile(url, filename, mimeType){
@@ -38,12 +85,9 @@ angular.module('adminModule')
 
         function readFile(evt) {
             var files = evt.target.files; // FileList object
-
             // Obtenemos la imagen del campo "file".
             for (var i = 0, f; f = files[i]; i++) {
-
                 var reader = new FileReader();
-
                 reader.onload = (function(e) {
                     document.getElementById('imgAddIcon').style.display = "none";
                     document.getElementById('img').style.display = "block";
@@ -63,5 +107,4 @@ angular.module('adminModule')
             }, 1000);
         }
         document.getElementById('file-upload').addEventListener('change', readFile, false);
-
     });
